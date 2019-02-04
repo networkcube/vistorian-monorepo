e794b5424ba8059e4cb8d753805386f5b1bdf2bd
Alexandra Ibarra <alexasofia@hotmail.com>
2019-02-04 07:48:56 -0300

    add jquery to html

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
         extensions: ['.tsx', '.ts', '.js']
     },
     output: {
+        libraryTarget: "umd",
         filename: 'index.js',
         path: path.resolve(__dirname, 'lib')
     }
