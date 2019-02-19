fff6dd5a04864bfe366bbe5f5a972061cff533af
Alexandra Ibarra <alexasofia@hotmail.com>
2019-02-19 17:17:06 +0100

    fix url repo

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/index.ts', 
     devtool: 'inline-source-map',
     plugins: [
