b94ceee98240b05c4b0c162092f19dcf9b3c87e3
alkadi-mh <alkadi_mh@hotmail.com>
2021-02-02 16:57:24 -0800

    trying fix

--- web/src/dataview.ts
+++ web/src/dataview.ts
@@ -1071,9 +1071,9 @@ export function updateEntryToLocationTableOSM(index: number, geoname: string, lo
         fullGeoNames.push(geoname);
         var xhr: any = $.ajax({
             url: "https://nominatim.openstreetmap.org/search",
-            headers: {  'Access-Control-Allow-Origin': 'https://www.vistorian.online/' , 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE','Access-Control-Allow-Credentials':'true','Access-Control-Allow-Headers': '*'},
+            headers: {  'Access-Control-Allow-Origin': '*' , 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, PUT, DELETE','Access-Control-Allow-Headers': '*'},
             data: {format: "json", limit: "1", q: geoname.split(',')[0].trim()},
-            dataType: 'json'
+            dataType: 'jsonp'
         })
             .done(function (data: any, text: any, XMLHttpRequest: any) {
                 var entry: any;
