d2eba15706a3e6ae9012887be3cbe6744871b10e
alkadi-mh <alkadi_mh@hotmail.com>
2021-02-05 20:10:20 -0800

    user activity logs

--- web/bookmarkLogConsent.js
+++ web/bookmarkLogConsent.js
@@ -59,6 +59,7 @@ function turnOnLogging(){
     bookmarksTool.style.display = "block";
     document.getElementById('myModal').style.display = "none";
 }
+
 function checkLogStatus(){
     if (localStorage.getItem("acceptLogging")){
         var checkBox = document.getElementById("consentOnoffswitch");
@@ -144,3 +145,194 @@ $(function () {
     e.preventDefault();
   });
 });
+
+// Detecting inactive users and nudging them 
+// Credit (with modification): https://css-tricks.com/detecting-inactive-users/ 
+
+const INACTIVE_USER_TIME_THRESHOLD = 15000; //300000 = 4 minutes
+const USER_ACTIVITY_THROTTLER_TIME = 15000 ; // 60000= 1 minute throttler
+
+let userActivityTimeout = null;
+let userActivityThrottlerTimeout = null;
+let isInactive = false;
+
+activateActivityTracker();
+
+
+//register the interactions' events with the function responsible
+function activateActivityTracker() {
+  window.addEventListener("mousemove", userActivityThrottler);
+  window.addEventListener("click", userActivityThrottler);
+  window.addEventListener("scroll", userActivityThrottler);
+  window.addEventListener("keydown", userActivityThrottler);
+  window.addEventListener("resize", userActivityThrottler);
+ window.addEventListener("beforeunload", inactiveUserAction);
+ 
+}
+function  deactivateActivityTracker() {
+  window.removeEventListener("mousemove", userActivityThrottler);
+  window.removeEventListener("click", userActivityThrottler);
+  window.removeEventListener("scroll", userActivityThrottler);
+  window.removeEventListener("keydown", userActivityThrottler);
+  window.removeEventListener("resize", userActivityThrottler);
+ // window.removeEventListener("onbeforeunload", inactiveUserAction);
+}
+
+//When the user interacts with the APP
+function resetUserActivityTimeout() {
+  clearTimeout(userActivityTimeout);
+
+  userActivityTimeout = setTimeout(() => {
+    userActivityThrottler();
+    inactiveUserAction();
+  }, INACTIVE_USER_TIME_THRESHOLD);
+}
+
+function inactiveUserAction() {
+  isInactive = true;//isActive=false
+  
+ document.getElementById('popupFeedbackForm').style.display="block";
+ trace.event('log_13', 'no activity detected', 'current web page', window.location.pathname);
+
+
+}
+
+
+function userActivityThrottler() {
+  if (isInactive) {
+    isInactive = false; //isActive=true
+    resetUserActivityTimeout();
+  }
+
+  if (!userActivityThrottlerTimeout) {
+    userActivityThrottlerTimeout = setTimeout(() => {
+      resetUserActivityTimeout();
+      clearTimeout(userActivityThrottlerTimeout);
+    }, USER_ACTIVITY_THROTTLER_TIME);
+  }
+}
+
+function disablingFeedbackPopup(chk){
+  if (chk.checked)
+    localStorage.setItem("stopFeedbackPopup", "true");
+  
+
+  if (localStorage.getItem("stopFeedbackPopup")==true){
+    deactivateActivityTracker();
+    clearTimeout(userActivityTimeout);
+    clearTimeout(userActivityThrottlerTimeout);
+  }
+  else
+    userActivityThrottler();
+
+  resetFeedbackForm();
+}
+
+function displaySubOptions(clickedButton){
+  let btns=document.getElementsByClassName('feedbackMenuButton');
+   for (var i=0;i<btns.length;i++){
+      if (clickedButton.value==btns[i].value)
+          clickedButton.style.backgroundColor= "#FF7F50";                     
+      else 
+          btns[i].style.backgroundColor= "#bbb"; 
+  }
+  if (clickedButton.value=="Analyzing Data")
+      document.getElementById("checkboxes_group_div").style.display="inline-block";
+  else
+      document.getElementById("checkboxes_group_div").style.display="none";
+  if (clickedButton.value=="Other")
+      document.getElementById("OtherType_div").style.display="inline-block";
+  else
+      document.getElementById("OtherType_div").style.display="none"; 
+}
+
+
+// Logging Feedback from the general feedback gadget
+function LoggingGeneralFeedback(){
+  let tempLog=false;
+
+  //check if the logging enabled, otherwise turn it temporarlly on for logging feedback
+  if (localStorage.getItem("acceptLogging")==false)
+      tempLog=true;
+
+  if (tempLog)
+    localStorage.setItem("acceptLogging", "true");
+
+    let btns=document.getElementsByClassName('menuButtonGadget');
+    for (var i=0;i<btns.length;i++)
+      //Set main type of usage
+       if (btns[i].style.backgroundColor=="#FF7F50")
+        trace.event('log_14', 'general feedback - bookmark type', btns[i].value, window.location.pathname);
+        //Set analysis type - if selected-
+        
+        //add general feedback
+        trace.event('log_15', 'General feedback', document.getElementById('feedback_text').value, document.location.pathname);
+      
+
+
+
+  if (tempLog)
+    localStorage.setItem("acceptLogging", "false");
+
+}
+
+
+
+// Logging Feedback from the popup questionairre
+function LoggingFeedback(){
+  let tempLog=false;
+
+  //check if the logging enabled, otherwise turn it temporarlly on for logging feedback
+  if (localStorage.getItem("acceptLogging")==false)
+      tempLog=true;
+
+  if (tempLog)
+    localStorage.setItem("acceptLogging", "true");
+
+    let btns=document.getElementsByClassName('feedbackMenuButton');
+    for (var i=0;i<btns.length;i++){
+      //Set main type of usage
+       if (btns[i].style.backgroundColor=="#FF7F50"){
+        trace.event('log_14', 'popup feedback - bookmark type', btns[i].value, window.location.pathname);
+        //Set analysis type - if selected-
+        if (btns[i].value=="Analyzing Data"){
+          let chks=document.getElementsByName('chks_feebackForm');
+          for (var i=0;i<chks.length;i++){
+            if (chks[i].checked)
+                trace.event('bkm_6', ' bookmark analysis type selected', chks[i].value, window.location.pathname);
+          }
+        }
+        
+        //check if type was other
+        if (btns[i].value=="Other")
+          trace.event('bkm_3', ' bookmark type specified', 'Other: ' + document.getElementById('txt_other').value, window.location.pathname);
+       }
+        //add general feedback
+        trace.event('log_15', 'feedback_popup', document.getElementById('feedback_text_popup').value, document.location.pathname);
+      }
+
+
+
+  if (tempLog)
+    localStorage.setItem("acceptLogging", "false");
+
+}
+
+function resetFeedbackForm(){
+  let btns=document.getElementsByClassName('feedbackMenuButton');
+    for (var i=0;i<btns.length;i++)
+      btns[i].style.backgroundColor="#bbb";
+  
+  let chks=document.getElementsByName('chks_feebackForm');
+      for (var i=0;i<chks.length;i++)
+        chks[i].checked=false;
+  document.getElementById("checkboxes_group_div").style.display="none";
+
+
+    document.getElementById('txt_other').value="";
+    document.getElementById('feedback_text_popup').value="";
+    document.getElementById("OtherType_div").style.display="none"; 
+
+    document.getElementById('popupFeedbackForm').style.display='none';
+  
+}
