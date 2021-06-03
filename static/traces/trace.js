48798e4409fcde35704ffb243b8c65e49c875c85
bbach <bbach@exseed.ed.ac.uk>
2021-06-03 14:44:29 +0100

    fix symmetry issue

--- static/traces/trace.js
+++ static/traces/trace.js
@@ -94,7 +94,7 @@
 	    httpRequest.setRequestHeader("Accept", "text/plain");
 	    //    httpRequest.setRequestHeader("Content-Length", json.length);
 	}
-	// httpRequest.send(json);
+	httpRequest.send(json);
     };
 
     var sendLogs = function() {
