f35a52910d3ff26d2ed8494a4fdc9a4d0c6baecb
James Scott-Brown <james@jamesscottbrown.com>
2021-12-13 12:31:54 +0000

    minify code during build

--- packages/vistorian-core/rollup.config.js
+++ packages/vistorian-core/rollup.config.js
@@ -2,6 +2,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import sourcemaps from "rollup-plugin-sourcemaps";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: "build/src/index.js",
@@ -18,5 +19,5 @@ export default {
       swiftset: "Set",
     },
   },
-  plugins: [nodeResolve(), commonjs(), json(), sourcemaps()],
+  plugins: [nodeResolve(), commonjs(), json(), sourcemaps(), terser()],
 };
--- packages/vistorian-matrix/webpack.config.js
+++ packages/vistorian-matrix/webpack.config.js
@@ -1,39 +1,43 @@
-const path = require('path');
-const CircularDependencyPlugin = require('circular-dependency-plugin');
+const path = require("path");
+const CircularDependencyPlugin = require("circular-dependency-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.ts', 
-    devtool: 'inline-source-map',
-    externals: {
-        science: 'science'
-    },
-    plugins: [
-        new CircularDependencyPlugin({
-            exclude: /a\.js|node_modules/,
-            failOnError: true,
-            cwd: process.cwd(),
-        })
+  mode: "production",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  externals: {
+    science: "science",
+  },
+  plugins: [
+    new CircularDependencyPlugin({
+      exclude: /a\.js|node_modules/,
+      failOnError: true,
+      cwd: process.cwd(),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.ts|\.tsx$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.ts|\.tsx$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    devServer: {
-        contentBase: './'
-    },
-    output: {
-        library: "matrix",
-        libraryTarget: "umd",
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'lib')
-    }
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    contentBase: "./",
+  },
+  output: {
+    library: "matrix",
+    libraryTarget: "umd",
+    filename: "index.js",
+    path: path.resolve(__dirname, "lib"),
+  },
+  optimization: {
+    minimizer: [new UglifyJsPlugin()],
+  },
 };
