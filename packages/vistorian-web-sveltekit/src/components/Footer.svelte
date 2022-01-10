45744d1065adc9954c528d450ba1b2ca35563124
James Scott-Brown <james@jamesscottbrown.com>
2022-01-10 12:25:34 +0000

    auto-reformat Footer.svelte

--- packages/vistorian-web-sveltekit/src/components/Footer.svelte
+++ packages/vistorian-web-sveltekit/src/components/Footer.svelte
@@ -1,13 +1,21 @@
 <script>
 	const year = new Date().getFullYear();
-	const height = "40px";
+	const height = '40px';
 </script>
 
 <div id="footerDiv">
-	<a href="https://vishub.net/"><img src="./logos/logo-vishub.png" height={height} alt="Logo for VisHub group" /></a>
-	<a href="https://www.inria.fr/fr"><img src="./logos/logo-inria.png" height={height} alt="Logo for INRIA" /></a>
-	<a href="https://www.microsoft.com/en-us/research/"><img src="./logos/logo-msr.png" height={height} alt="Logo for Microsoft" /></a>
-	<a href="https://www.ed.ac.uk/"><img src="./logos/logo-edinburgh.png" height={height} alt="Logo for University of Edinburgh" /></a>
+	<a href="https://vishub.net/"
+		><img src="./logos/logo-vishub.png" {height} alt="Logo for VisHub group" /></a
+	>
+	<a href="https://www.inria.fr/fr"
+		><img src="./logos/logo-inria.png" {height} alt="Logo for INRIA" /></a
+	>
+	<a href="https://www.microsoft.com/en-us/research/"
+		><img src="./logos/logo-msr.png" {height} alt="Logo for Microsoft" /></a
+	>
+	<a href="https://www.ed.ac.uk/"
+		><img src="./logos/logo-edinburgh.png" {height} alt="Logo for University of Edinburgh" /></a
+	>
 </div>
 
 <style>
