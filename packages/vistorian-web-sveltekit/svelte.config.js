bf3b4b2e1acd7eb52c94225f55d15c3bbe8d032a
James Scott-Brown <james@jamesscottbrown.com>
2022-03-09 16:23:24 +0000

    re-format svelte config file

--- packages/vistorian-web-sveltekit/svelte.config.js
+++ packages/vistorian-web-sveltekit/svelte.config.js
@@ -1,25 +1,25 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		}),
-		
-		prerender: {
-			default: true
-		}
-	}
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    adapter: adapter({
+      // default options are shown
+      pages: "build",
+      assets: "build",
+      fallback: null
+    }),
+
+    prerender: {
+      default: true
+    }
+  }
 };
 
 export default config;
